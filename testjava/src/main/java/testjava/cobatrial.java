c5e8c6431633f12615abd917d4eae9799d3b7e1a
widia-a <wwiidiiaa@gmail.com>
2022-11-08 10:23:03 +0700

    trial

---
+++ testjava/src/main/java/testjava/cobatrial.java
@@ -0,0 +1,13 @@
+package testjava;
+
+/*
+@author DELL a.k.a. widia
+Created with eclipse version
+Created on 08-11-2022 10:22:19
+Last modified on 08-11-2022 10:22:19
+Version 1.0
+*/
+
+public class cobatrial {
+
+}
